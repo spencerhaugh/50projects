f4ea987b9620095d0ff8f59540c4b8f1e11338eb
Spencer Haugh <spencer.haugh@gmail.com>
2021-02-11 11:39:57 -0500

    adjusted line length to 99% to completely hide end behind circle 4

--- 2-progress_steps/script.js
+++ 2-progress_steps/script.js
@@ -6,6 +6,10 @@ const circles = document.querySelectorAll('.circle');
 // current active stage/progress
 let currentActive = 1;
 
+//========
+// Buttons
+//========
+
 // next button 
 next.addEventListener('click', () => {
     currentActive++;
@@ -28,6 +32,7 @@ prev.addEventListener('click', () => {
     update();
 });
 
+// Update function logic for added/removing active class, disbale buttons as necessary
 const update = () => {
     circles.forEach((circle, index) => {
         if (index < currentActive) {
@@ -40,5 +45,15 @@ const update = () => {
     const actives = document.querySelectorAll('.active')
 
     // set progress class width to correct active circle position (33%, 66%, 100%)
-    progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 +'%'
+    progress.style.width = (actives.length - 1) / (circles.length - 1) * 99 +'%'
+
+    // disable buttons when at first or last option  
+    if (currentActive === 1) {
+        prev.disabled = true;
+    } else if (currentActive === circles.length) {
+        next.disabled = true;
+    } else {
+        prev.disabled = false;
+        next.disabled = false;
+    }
 }

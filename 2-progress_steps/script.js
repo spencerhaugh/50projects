c6539b2853dd7a5e2d86a14ecc9400e59184eed7
Spencer Haugh <spencer.haugh@gmail.com>
2021-02-11 11:25:37 -0500

    javascript listeners added. active class function added

--- 2-progress_steps/script.js
+++ 2-progress_steps/script.js
@@ -2,3 +2,38 @@ const progress = document.getElementById('progress');
 const prev = document.getElementById('prev');
 const next = document.getElementById('next');
 const circles = document.querySelectorAll('.circle');
+
+// current active stage/progress
+let currentActive = 1;
+
+// next button 
+next.addEventListener('click', () => {
+    currentActive++;
+
+    if (currentActive > circles.length) {
+        currentActive = circles.length;
+    }
+
+    update();
+});
+
+// prev button
+prev.addEventListener('click', () => {
+    currentActive--;
+
+    if (currentActive < 1) {
+        currentActive = 1;
+    }
+
+    update();
+});
+
+const update = () => {
+    circles.forEach((circle, index) => {
+        if (index < currentActive) {
+            circle.classList.add('active');
+        } else {
+            circle.classList.remove('active');
+        }
+    })
+}

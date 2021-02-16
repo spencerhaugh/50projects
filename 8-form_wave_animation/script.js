b5a1262c6e76f7982e43e91eed5a1b3aa169450d
Spencer Haugh <spencer.haugh@gmail.com>
2021-02-16 10:47:53 -0500

    Project complete

--- 8-form_wave_animation/script.js
+++ 8-form_wave_animation/script.js
@@ -3,6 +3,8 @@ const labels = document.querySelectorAll('.form-control label');
 labels.forEach(label => {
     label.innerHTML = label.innerText // grabs label text (eg "Email")
         .split('') // Split into an array of letters
-        .map((letter, index) => `<span>${letter}</span>`) // Map each letter to a span element
-        .join('') // join all the spans into a code block
+        .map((letter, index) => `<span style="transition-delay:${index * 20}ms">${letter}</span>`) 
+        // Map each letter to a span element. 
+        // Set inline style "transition-delay to the idex times 20ms to create incrementing delay"
+        .join('') // Join all the spans into a code block
 })

c85e7b06ba52afaaab9813c4440fbb93eeade312
Spencer Haugh <spencer.haugh@gmail.com>
2021-04-09 15:09:46 -0400

    Rough out JS logic

---
+++ 29-double_heart_click/script.js
@@ -0,0 +1,8 @@
+const loved = document.getElementById('loved');
+
+loved.addEventListener('dblclick', (e) => {
+    // get click position
+
+    // insert icon html element at point of click
+    // setTimeout to remove element after animation
+})

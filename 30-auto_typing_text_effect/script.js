43bcb400007efa30607d044aead4006a104378a7
Spencer Haugh <spencer.haugh@gmail.com>
2021-04-12 12:00:57 -0400

    Project complete

--- 30-auto_typing_text_effect/script.js
+++ 30-auto_typing_text_effect/script.js
@@ -7,7 +7,7 @@ const text = "Welcome To The Internet"
 
 //Animation speed controls
 let index = 1;
-let speed = 200 / speedEl.value
+let speed = 300 / speedEl.value;
 
 //Call writeText function
 writeText();
@@ -15,9 +15,15 @@ writeText();
 
 //Functions
 
+    //Text Controller
 function writeText() {
     textEl.innerText = text.slice(0, index);
     index++;
 
+    if (index > text.length) index = 1; // Repeat, if needed
+
     setTimeout(writeText, speed)
 }
+
+    //Speed Controller
+speedEl.addEventListener('input', (e) => speed = 300 / e.target.value);

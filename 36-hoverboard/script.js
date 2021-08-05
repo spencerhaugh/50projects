d32149803cbd9569ef80d2cbf610e62610e6d86f
Spencer Haugh <spencer.haugh@gmail.com>
2021-08-05 11:22:13 -0400

    Design updates

--- 36-hoverboard/script.js
+++ 36-hoverboard/script.js
@@ -1,20 +1,23 @@
 const container = document.getElementById('container');
-const colors = ['red', 'purple', 'indigo', 'violet', 'brickred', 'orange'];
-const squares = 500;
+const colors = ['#D1C4E9', '#B39DDB', '#9575CD', '#7E57C2', '#673AB7', '#5E35B1', '#512DA8', '#4527A0', '#311B92'];
+const squares = 506;
 
 const changeColor = (square) => {
-    square.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+    let randomColor = colors[Math.floor(Math.random() * colors.length)];
+    square.style.backgroundColor = randomColor;
+    square.style.boxShadow = `0 0 2px ${randomColor}, 0 0 10px ${randomColor}`;
 };
 
 const removeColor = (square) => {
     square.style.backgroundColor = '#1d1d1d';
+    square.style.boxShadow = '0 0 2px #000';
 };
 
 for (let i = 0; i < squares; i++) {
     const square = document.createElement('div');
     square.classList.add('square');
 
-    square.addEventListener('mouseenter', () => {
+    square.addEventListener('mouseover', () => {
         changeColor(square)
     });
 
